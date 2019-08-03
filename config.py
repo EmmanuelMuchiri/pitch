422b57efb371cb8322543b6778e0f6a1f265177d
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-04 00:46:49 +0300

    created a directory to store users profile photos and linked it in config.py file

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
