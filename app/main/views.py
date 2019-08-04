e9b7c6c141c4b3d676e01d54bc1bcd7b986e78ea
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-04 22:14:07 +0300

    Confirmed that one can successfully Add a pitch and view the pitch added

--- app/main/views.py
+++ app/main/views.py
@@ -44,6 +44,7 @@ def new_pitch():
 
 
 @main.route('/posts')
+@login_required
 def post_pitch():
 
     pitches = Pitch.query.all()
@@ -64,65 +65,6 @@ def post_review(id):
         new_review.save_review()
         return redirect(url_for('.post_review',id=pitch.id))
     return render_template('reviews.html',form=form, pitch=pitch)
-
-
-
-# @main.route('/category/pitch/new/<int:id>', methods = ["GET", "POST"])
-# @login_required
-# def new_pitch(id):
-#     '''
-#     view category that returns a form to create a pitch
-#     '''
-#     form = PitchForm()
-#     category = Category.query.filter_by(id = id).first()
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         post = form.post.data
-
-#         # pitch instance
-#         new_pitch = Pitch(category_id = category.id, title = title, post = post, user = current_user)
-
-#         # save pitch
-#         new_pitch.save_pitch()  
-#         return redirect(url_for('.category', id = category.id))
-#     title = f'{category.name} pitches'
-#     return render_template('new_pitch.html', title = title, pitch_form = form, category = category)
-
-# @main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
-# @login_required
-# def new_review(id):
-#     '''
-#     view category that returns a form to create a new review
-#     '''
-#     form = ReviewForm()
-#     pitch = Pitch.query.filter_by(id = id).first()
-#     if form.validate_on_submit():
-#         review = form.review.data
-
-#         # review instance
-#         new_review = Review(pitch_id = pitch.id, post_review = review, user = current_user)
-
-#         # save review 
-#         new_review.save_review()
-#         return redirect(url_for('.reviews', id = pitch.id ))
-
-#     title = f'{pitch.title} review'
-#     return render_template('new_review.html', title = title, review_form = form, pitch = pitch)
-
-# @main.route('/pitch/reviews/<int:id>')
-# def reviews(id):
-#     '''
-#     viw category that returns all reviews for a pitch
-#     '''
-#     pitch = Pitch.query.get(id)
-#     review = Review.get_reviews(pitch.id)
-#     title = f'{pitch.title} review'
-
-#     return render_template('reviews.html', title = title, pitch = pitch, review = review)
-
-
-
-
 
 
 @main.route('/user/<uname>')
