3dae301f102f32348438d3c3eadd4cb50874b7d4
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-04 16:19:29 +0300

    confirmed that the signup functionality is sending an email upon login

--- app/__init__.py
+++ app/__init__.py
@@ -37,11 +37,11 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
 
     # setting config
     # from .request import configure_request
     # configure_request(app)
     #  # configure UploadSet
-    # configure_uploads(app,photos)
+    configure_uploads(app,photos)
     return app
