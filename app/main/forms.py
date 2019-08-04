588b765b3fdfa71d9b5f8b6ea2e3c59a1bcc43a8
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-04 21:12:45 +0300

    added the pitchform

--- app/main/forms.py
+++ app/main/forms.py
@@ -10,3 +10,13 @@ class ReviewForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title', validators=[Required()])
+    pitch = TextAreaField('Your Pitch', validators=[Required()])
+    # category = SelectField('Category', choices=[('Business','Business'),('Event','Event'),('Technology','Technology')],validators=[Required()])
+    submit = SubmitField('PITCH')
+
+# class CommentForm(FlaskForm):
+#     title = StringField('Comment')
+#     comment = TextAreaField('Enter your comment')
+#     submit = SubmitField('submit')
