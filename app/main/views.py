59828cc08d2c9bc16729b75cb67b190a6e1329cc
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-04 16:27:36 +0300

    trying to update the routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
-# from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm,UpdateProfile
 from ..models import Review,User
 from flask_login import login_required,current_user
