43d59af08df7e8aec71586e45be9ded1f8696e96
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-04 16:49:49 +0300

    defined two routes and updated their render templates as well.One for reviews and the other one for new reveiews

--- app/main/views.py
+++ app/main/views.py
@@ -48,6 +48,42 @@ def new_pitch(id):
     title = f'{category.name} pitches'
     return render_template('new_pitch.html', title = title, pitch_form = form, category = category)
 
+@main.route('/pitch/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_review(id):
+    '''
+    view category that returns a form to create a new review
+    '''
+    form = ReviewForm()
+    pitch = Pitch.query.filter_by(id = id).first()
+    if form.validate_on_submit():
+        review = form.review.data
+
+        # review instance
+        new_review = Review(pitch_id = pitch.id, post_review = review, user = current_user)
+
+        # save review 
+        new_review.save_review()
+        return redirect(url_for('.reviews', id = pitch.id ))
+
+    title = f'{pitch.title} review'
+    return render_template('new_review.html', title = title, review_form = form, pitch = pitch)
+
+@main.route('/pitch/reviews/<int:id>')
+def reviews(id):
+    '''
+    viw category that returns all reviews for a pitch
+    '''
+    pitch = Pitch.query.get(id)
+    review = Review.get_reviews(pitch.id)
+    title = f'{pitch.title} review'
+
+    return render_template('reviews.html', title = title, pitch = pitch, review = review)
+
+
+
+
+
 
 @main.route('/user/<uname>')
 @login_required
