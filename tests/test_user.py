59f29993c183de2f5d2584c49323fd529145033c
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-05 00:36:09 +0300

    ran 3 basic tests and ensured that they are working perfectly

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,12 +5,9 @@ class UserModelTest(unittest.TestCase):
     def setUp(self):
         self.new_user = User(password = 'password')
 
-    def test_password_setter(self):
-        self.assertTrue(self.new_user.pass_secure is not None)
-
     def test_no_access_password(self):
             with self.assertRaises(AttributeError):
                 self.new_user.password
 
-        def test_password_verification(self):
-            self.assertTrue(self.new_user.verify_password('password'))
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('password'))
