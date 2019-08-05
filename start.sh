2a461c3bda971690916cab8f85560cd767d3d948
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-05 11:08:27 +0300

    made changes to my config.py to try deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export SECRET_KEY='<Flask WTF Secret Key>'
 export MAIL_USERNAME='emmahg6@gmail.com'
 export MAIL_PASSWORD='Emmanuel@71270706915605'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://mannuh:123@localhost/pitch'
+
 python3.6 manage.py server
