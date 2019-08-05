7282230eb3842726c35237f6b75f6ab2638acf93
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-05 10:50:15 +0300

    redesigned my schema and linked it with my uri

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -19,7 +18,8 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
+
     
 
 class TestConfig(Config):
