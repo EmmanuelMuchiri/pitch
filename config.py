a75a2f631d26296256f58faa8389060687c36170
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-06 12:38:34 +0300

    updated the config.py and ran the start.sh and ensured that it is running perfectly

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class Config:
 
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     DEBUG = True
 
@@ -31,7 +31,7 @@ class TestConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mannuh:123@localhost/pitch'
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     DEBUG = True
 
